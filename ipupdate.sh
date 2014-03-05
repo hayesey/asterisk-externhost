08ec97541bc8eb0263c832984873fb3d6c443032
Paul Hayes <paul@polog40.co.uk>
2014-03-05 17:16:11 +0000

    will work for both sip.conf and pjsip.conf now but fixed file names

--- ipupdate.sh
+++ ipupdate.sh
@@ -1,28 +1,63 @@
 #!/bin/bash
-filename="/etc/asterisk/sip.conf"
+
+filestodo=()
 hostname="$1"
 
-if [ "$2" == "silent" ]; then 
+if [ -z "$1" ] || [ "$1" == "silent" ]
+then
+  echo "fatal error: ARG 1 must be the hostname to lookup, the silent keyword can only be ARG 2"
+  exit 1
+fi
+
+if [ $(ls /etc/asterisk | egrep "^sip.conf$") ]
+then
+ filestodo+=("sip.conf")
+fi
+
+if [ $(ls /etc/asterisk | egrep "^pjsip.conf$") ]
+then
+ filestodo+=("pjsip.conf")
+fi
+
+if [ -z "$filestodo[0]" ]
+then
+  echo "fatal error: neither sip.conf or pjsip.conf found, nothing to compare"
+  exit 1
+fi
+
+if [ "$2" == "silent" ] 
+then 
   debug=0
 else
   debug=1
 fi
 
-currentip=$(grep externaddr $filename)
-currentip=${currentip##*=}
-if [ "$debug" == 1 ]; then echo "ip read from file $filename is: $currentip";fi
-
-newip=$(host $hostname)
+newip=$(host -t A $hostname)
 newip=${newip##* }
-
 if [ "$debug" == 1 ]; then echo "ip from hostname $hostname is: $newip";fi
 
-if [ "$newip" != "$currentip" ]; then
-  if [ "$debug" == 1 ]; then echo "ips different";fi
-  cp $filename $filename.old
-  sed "s/externaddr=$currentip/externaddr=$newip/g" $filename.old > $filename
-  rm -f $filename.old
-  /usr/sbin/asterisk -rx "sip reload" &> /dev/null
-else
-  if [ "$debug" == 1 ]; then echo "no change";fi
-fi
+for thisfile in ${filestodo[@]}
+do
+  filename="/etc/asterisk/$thisfile"
+  if [[ $thisfile == *pjsip* ]] 
+  then 
+    currentip=$(grep external_signaling_address $filename)
+  else
+    currentip=$(grep externaddr $filename)
+  fi
+
+  currentip=${currentip##*=}
+  if [ "$debug" == 1 ]; then echo "ip read from file $filename is: $currentip";fi
+
+  if [ "$newip" != "$currentip" ] 
+  then
+    if [ "$debug" == 1 ]; then echo "ips different";fi
+    cp $filename $filename.old
+    sed "s/$currentip/$newip/g" $filename.old > $filename
+    rm -f $filename.old
+    /usr/sbin/asterisk -rx "sip reload" &> /dev/null
+  else
+    if [ "$debug" == 1 ]; then echo "no change";fi
+  fi
+done
+exit 0
